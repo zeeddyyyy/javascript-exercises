36f216b70652a350714a4ee145a310b18c9206a6
zeeddyyyy <87855090+zeeddyyyy@users.noreply.github.com>
2026-02-14 22:19:43 +0100

    complete palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+   let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, "");
+  let reversedStr = cleanStr.split('').reverse().join('');
+  return cleanStr === reversedStr;
 };
 
 // Do not edit below this line
