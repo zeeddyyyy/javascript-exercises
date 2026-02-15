864b7c3910a0afd92838e6b04706d648bb0c8745
zeeddyyyy <87855090+zeeddyyyy@users.noreply.github.com>
2026-02-15 19:35:45 +0100

    complete findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  const sorted = arr.map(person => {
+    
+    const death = person.yearOfDeath || new Date().getFullYear();
+    const birth = person.yearOfBirth;
+    
+    return {
+      name: person.name,
+      age: death - birth
+    };
+  }).sort((a, b) => b.age - a.age); 
 
+  return sorted[0];
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
