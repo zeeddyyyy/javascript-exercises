339f285e5f3638843a9c70b94e4047ff96802857
zeeddyyyy <87855090+zeeddyyyy@users.noreply.github.com>
2026-02-15 17:54:29 +0100

    complete getTheTitles exercise

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  const titles = arr.map(item => item.title);
+  return titles;
 };
-
 // Do not edit below this line
 module.exports = getTheTitles;
